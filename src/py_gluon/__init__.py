45ffa81efee8a2c6fb7d70467c5b04b746b4390f
Luca D'Angelo <ldgaetano@protonmail.com>
2026-01-05 01:11:35 -0500

    feat: v0.2.0 publish to pypi

--- src/py_gluon/__init__.py
+++ src/py_gluon/__init__.py
@@ -1,3 +1,3 @@
 """py-gluon: Gluon simulator library for python. """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
---
+++ src/py_gluon/reactors/__init__.py
@@ -0,0 +1,6 @@
+"""Gluon reactors."""
+
+from py_gluon.reactors.gluon_reactor import GluonReactor
+from py_gluon.reactors.gluon_z_reactor import GluonZReactor
+
+__all__ = ["GluonReactor", "GluonZReactor"]
